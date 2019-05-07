72ff851edf72bab6947fa079706d656108a6ad0b
Oskar <omikolaj1@gmail.com>
2019-05-07 06:00:07 -0400

    The about component now has a parallax image going across with deep fade from the top and bottom. The currently active route is now highlighted with accent color

--- src/app/static/about/about.component.ts
+++ src/app/static/about/about.component.ts
@@ -1,84 +1,88 @@
-import { Component, OnInit } from '@angular/core';
-import 
-{ 
+import { Component, OnInit } from "@angular/core";
+import {
   trigger,
   state,
   style,
-  transition, 
-  animate, 
-  keyframes, 
-  query, 
-  stagger, 
-  group, 
-  animateChild 
-} from '@angular/animations';
+  transition,
+  animate,
+  keyframes,
+  query,
+  stagger,
+  group,
+  animateChild
+} from "@angular/animations";
 
-import { ROUTE_ANIMATIONS_ELEMENTS } from '../../core/animations/route.animations';
+import { ROUTE_ANIMATIONS_ELEMENTS } from "../../core/animations/route.animations";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss'],
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
   animations: [
-    trigger('flyInOut', [      
-      transition(':enter', [
-        group([          
-          query('h1, .center, .info, .animations', [
-            style({opacity: 0}),
-            stagger(290, [
-              animate('.3s ease-in-out', style({ opacity: 1}))
-            ])
-          ], {optional: true}),
-          query('@fadeInOut', animateChild(), { optional: true })         
-        ])        
+    trigger("flyInOut", [
+      transition(":enter", [
+        group([
+          query(
+            "h1, .center, .info, .animations",
+            [
+              style({ opacity: 0 }),
+              stagger(290, [animate(".3s ease-in-out", style({ opacity: 1 }))])
+            ],
+            { optional: true }
+          ),
+          query("@fadeInOut", animateChild(), { optional: true })
+        ])
       ])
     ]),
-    trigger('fadeInOut', [
-      state('fadeIn', style({ opacity: 1})),
-      transition(':enter', [
-        style({ opacity: 0 }),        
-          animate('1.0s ease-in-out', keyframes([
-            style({ opacity: .0, offset: .2 }),
-            style({ opacity: .2, offset: .6 }),
-            style({ opacity: .4, offset: .8 }),
-            style({ opacity: .99, offset: 1 })
-        ]))        
+    trigger("fadeInOut", [
+      state("fadeIn", style({ opacity: 1 })),
+      transition(":enter", [
+        style({ opacity: 0 }),
+        animate(
+          "1.0s ease-in-out",
+          keyframes([
+            style({ opacity: 0.0, offset: 0.2 }),
+            style({ opacity: 0.2, offset: 0.6 }),
+            style({ opacity: 0.4, offset: 0.8 }),
+            style({ opacity: 0.99, offset: 1 })
+          ])
+        )
       ])
     ]),
-    trigger('pulsing', [
-      state('inactive', style({ opacity: 1, color: 'white' })),
-      state('active', style({ opacity: 1, color: 'white' })),
-      transition('active <=> inactive', [        
-        animate('4s .5s ease-in-out', keyframes([
-          style({ opacity: .2, offset: 0.2}),
-          style({ opacity: .4, offset: 0.4}),
-          style({ opacity: .6, offset: 0.6}),
-          style({ opacity: .8, offset: 0.75}),
-          style({ opacity: 1, offset: 0.9})
-        ]))
+    trigger("pulsing", [
+      state("inactive", style({ opacity: 1, color: "white" })),
+      state("active", style({ opacity: 1, color: "white" })),
+      transition("active <=> inactive", [
+        animate(
+          "4s .5s ease-in-out",
+          keyframes([
+            style({ opacity: 0.2, offset: 0.2 }),
+            style({ opacity: 0.4, offset: 0.4 }),
+            style({ opacity: 0.6, offset: 0.6 }),
+            style({ opacity: 0.8, offset: 0.75 }),
+            style({ opacity: 1, offset: 0.9 })
+          ])
+        )
       ])
-    ])    
+    ])
   ]
 })
 export class AboutComponent implements OnInit {
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
   isIn: boolean = false;
-  pulsingState: string = 'inactive';  
-  constructor() { }
+  pulsingState: string = "inactive";
+  constructor() {}
 
   ngOnInit() {
     this.isIn = true;
-    this.pulsingState = 'active';
+    this.pulsingState = "active";
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.isIn = false;
   }
 
-  onDonePulsing(){
-    this.pulsingState = this.pulsingState === 'active' ? 'inactive' : 'active';
+  onDonePulsing() {
+    this.pulsingState = this.pulsingState === "active" ? "inactive" : "active";
   }
-
-  
-
 }
--- src/app/static/merchandise/merchandise-list/merchandise-item/merchandise-item.component.ts
+++ src/app/static/merchandise/merchandise-list/merchandise-item/merchandise-item.component.ts
@@ -1,37 +1,44 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { GearItem } from 'src/app/core/models/gear-item.model';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MerchandiseService } from 'src/app/core/services/merchandise/merchandise.service';
-import { Size } from 'src/app/core/models/gear-size.model';
-import { ROUTE_ANIMATIONS_ELEMENTS } from 'src/app/core/animations/route.animations';
+import { Component, OnInit, Input } from "@angular/core";
+import { GearItem } from "src/app/core/models/gear-item.model";
+import { ActivatedRoute, Router } from "@angular/router";
+import { MerchandiseService } from "src/app/core/services/merchandise/merchandise.service";
+import { Size } from "src/app/core/models/gear-size.model";
+import { ROUTE_ANIMATIONS_ELEMENTS } from "src/app/core/animations/route.animations";
 
 @Component({
-  selector: 'app-merchandise-item',
-  templateUrl: './merchandise-item.component.html',
-  styleUrls: ['./merchandise-item.component.scss']
+  selector: "app-merchandise-item",
+  templateUrl: "./merchandise-item.component.html",
+  styleUrls: ["./merchandise-item.component.scss"]
 })
 export class MerchandiseItemComponent implements OnInit {
-  @Input() gearItem: GearItem;  
-  sizes = Size;  
-  routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;   
+  @Input() gearItem: GearItem;
+  sizes = Size;
+  routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private merchandiseService: MerchandiseService 
-  ) { }
+    private merchandiseService: MerchandiseService
+  ) {}
 
-  ngOnInit() {        
-  }
+  ngOnInit() {}
 
-  onEditGearItem(){
-    this.router.navigate([{ outlets: { modal: [this.gearItem.ID, 'edit'] } }], { relativeTo: this.route })
-  }
-
-  onDeleteGearItem(){
-    this.merchandiseService.deleteGearItem(this.gearItem.ID).subscribe(
-      (deletedItem: boolean) => console.log('GEARITEM DELETED SUCCESS?: ', deletedItem)
+  onEditGearItem() {
+    this.router.navigate(
+      [
+        {
+          outlets: { modal: [this.gearItem.ID, "edit"] }
+        }
+      ],
+      { relativeTo: this.route }
     );
   }
 
+  onDeleteGearItem() {
+    this.merchandiseService
+      .deleteGearItem(this.gearItem.ID)
+      .subscribe((deletedItem: boolean) =>
+        console.log("GEARITEM DELETED SUCCESS?: ", deletedItem)
+      );
+  }
 }
