baec44c7fed1a55b678a860f890d81294736c103
Oskar <omikolaj1@gmail.com>
2019-05-09 08:36:05 -0400

    Modifyting carousel component to allow for lightbox display of images

--- src/app/static/merchandise/components/carousel/carousel.component.ts
+++ src/app/static/merchandise/components/carousel/carousel.component.ts
@@ -1,28 +1,85 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { GearItem } from 'src/app/core/models/gear-item.model';
-import { GearImage } from 'src/app/core/models/gear-image.model';
+import { Component, OnInit, Input } from "@angular/core";
+import { GearItem } from "src/app/core/models/gear-item.model";
+import { GearImage } from "src/app/core/models/gear-image.model";
+import "hammerjs";
+import {
+  NgxGalleryOptions,
+  NgxGalleryImage,
+  NgxGalleryAnimation,
+  NgxGalleryLayout
+} from "ngx-gallery";
 
 @Component({
-  selector: 'app-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  selector: "app-carousel",
+  templateUrl: "./carousel.component.html",
+  styleUrls: ["./carousel.component.scss"]
 })
 export class CarouselComponent implements OnInit {
   @Input() images: GearImage[] = [];
-  defaultImgURL: string = "../../../../assets/IMG_5585.JPG"
-  imagess = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
-  constructor() { }
+  defaultImgURL: string = "../../../../assets/IMG_5585.JPG";
+  imagess = [1, 2, 3].map(
+    () => `https://picsum.photos/900/500?random&t=${Math.random()}`
+  );
+
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
+  constructor() {}
 
   ngOnInit() {
+    this.galleryOptions = [
+      {
+        width: "360px",
+        height: "400px",
+        thumbnailsColumns: this.images.length,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        thumbnails: false,
+        previewSwipe: true,
+        previewCloseOnClick: true,
+        previewCloseOnEsc: true,
+        previewKeyboardNavigation: true,
+        previewAutoPlay: true,
+        previewAutoPlayInterval: 3000,
+        previewInfinityMove: true,
+        previewZoom: true
+      },
+      // max-width 800
+      {
+        breakpoint: 800,
+        width: "100%",
+        height: "600px",
+        imagePercent: 80,
+        thumbnailsPercent: 20,
+        thumbnailsMargin: 20,
+        thumbnailMargin: 20
+      },
+      // max-width 400
+      {
+        breakpoint: 400,
+        preview: false
+      }
+    ];
+
+    this.galleryImages = [
+      {
+        small: "../../../../assets/Leo.JPG",
+        medium: "../../../../assets/Leo.JPG",
+        big: "../../../../assets/Leo.JPG"
+      },
+      {
+        small: "../../../../assets/IMG_5585.JPG",
+        medium: "../../../../assets/IMG_5585.JPG",
+        big: "../../../../assets/IMG_5585.JPG"
+      }
+    ];
   }
 
-  displayImageSource(image: GearImage): string{    
-    if(image != null){
-      if(image.url != null){
+  displayImageSource(image: GearImage): string {
+    if (image != null) {
+      if (image.url != null) {
         return image.url;
       }
     }
     return this.defaultImgURL;
   }
-
 }
--- src/app/static/static.module.ts
+++ src/app/static/static.module.ts
@@ -1,32 +1,33 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AboutComponent } from './about/about.component';
-import { StaticRoutingModule } from './static.routing';
-import { LayoutModule } from '@angular/cdk/layout';
-import { SharedModule } from '../shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { TeamSignupFormComponent } from './about/components/team-signup-form/team-signup-form.component';
-import { CoreModule } from '../core/core.module';
-import { GalleryGridListComponent } from './gallery/gallery-grid-list.component';
-import { MerchandiseListComponent } from './merchandise/merchandise-list/merchandise-list.component';
-import { MerchandiseItemComponent } from './merchandise/merchandise-list/merchandise-item/merchandise-item.component';
-import { MerchandiseDialogContainerComponent } from './merchandise/merchandise-dialog-container/merchandise-dialog-container.component';
-import { MerchandiseDialogComponent } from './merchandise/merchandise-dialog-container/merchandise-dialog/merchandise-dialog.component';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { MerchandiseDialogService } from './merchandise/merchandise-dialog.service';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CarouselComponent } from './merchandise/components/carousel/carousel.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AboutComponent } from "./about/about.component";
+import { StaticRoutingModule } from "./static.routing";
+import { LayoutModule } from "@angular/cdk/layout";
+import { SharedModule } from "../shared/shared.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { TeamSignupFormComponent } from "./about/components/team-signup-form/team-signup-form.component";
+import { CoreModule } from "../core/core.module";
+import { GalleryGridListComponent } from "./gallery/gallery-grid-list.component";
+import { MerchandiseListComponent } from "./merchandise/merchandise-list/merchandise-list.component";
+import { MerchandiseItemComponent } from "./merchandise/merchandise-list/merchandise-item/merchandise-item.component";
+import { MerchandiseDialogContainerComponent } from "./merchandise/merchandise-dialog-container/merchandise-dialog-container.component";
+import { MerchandiseDialogComponent } from "./merchandise/merchandise-dialog-container/merchandise-dialog/merchandise-dialog.component";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { MerchandiseDialogService } from "./merchandise/merchandise-dialog.service";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { CarouselComponent } from "./merchandise/components/carousel/carousel.component";
+import { NgxGalleryModule } from "ngx-gallery";
 
 @NgModule({
   declarations: [
-    AboutComponent, 
-    TeamSignupFormComponent, 
-    GalleryGridListComponent, 
+    AboutComponent,
+    TeamSignupFormComponent,
+    GalleryGridListComponent,
     MerchandiseListComponent,
     MerchandiseItemComponent,
     MerchandiseDialogContainerComponent,
     MerchandiseDialogComponent,
-    CarouselComponent    
+    CarouselComponent
   ],
   entryComponents: [MerchandiseDialogComponent],
   imports: [
@@ -34,15 +35,16 @@ import { CarouselComponent } from './merchandise/components/carousel/carousel.co
     StaticRoutingModule,
     SharedModule,
     LayoutModule,
-    CommonModule,    
+    CommonModule,
     ReactiveFormsModule,
     CoreModule,
-    NgbModule
-  ], 
+    NgbModule,
+    NgxGalleryModule
+  ],
   providers: [
     MerchandiseDialogService,
     { provide: MatDialogRef, useValue: {} },
     { provide: MAT_DIALOG_DATA, useValue: {} }
   ]
 })
-export class StaticModule { }
+export class StaticModule {}
