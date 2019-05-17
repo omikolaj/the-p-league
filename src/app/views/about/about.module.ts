c2797731f0275bf9f870596ad787cc5f78800eab
Oskar <omikolaj1@gmail.com>
2019-05-17 07:27:13 -0400

    fixed issue with device detector module

--- src/app/views/about/about.module.ts
+++ src/app/views/about/about.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { DeviceDetectorModule } from "ngx-device-detector";
 import { AboutRoutingModule } from "./about-routing.module";
 import { AboutComponent } from "./about.component";
 import { TeamSignupFormModule } from "./components/team-signup-form/team-signup-form.module";
